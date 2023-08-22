d704512194675cc75c23aa93ba5b1d9c288e94a5
junsoo <choix712@umn.edu>
2023-08-22 18:13:28 +0900

    Servlet과 JSP패턴 구현: 그리고 한계점

---
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,42 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository(){
+
+    }
+
+    // 싱글톤으로 만들 때는 private으로 생성자를 만들어서 아무나 생성하는 것을 막아야함!
+
+    public Member save(Member member){
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
