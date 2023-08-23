a849b106f40fa9f6e7429ddde5d9a48f9c4b9e16
junsoo <choix712@umn.edu>
2023-08-23 23:08:53 +0900

    Spring MVC 방식으로 구현하기

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// handlerMapping, handleAdapter
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+
+        // viewResolver 논리적인 이름 -> 물리적인 이름 변환
+
+    }
+}
